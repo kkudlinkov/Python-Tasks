1f0f2fad540406392b282101524346f2bf2addbf
Nikolai Kudlinkov <kkudlinkov@mail.ru>
2023-05-18 13:58:41 +0300

    ADD: kispython task

--- KisPythonPrac/task5.3.py
+++ KisPythonPrac/task5.3.py
@@ -1,17 +1,17 @@
-def Eq(x, y):
-    return lambda row: row[x] == y
-
-def Ne(x, y):
-    return lambda row: row[x] != y
-
 def Value(x):
     return x
+
 
 def Field(x):
     return lambda row: row[x]
 
-def Filter(pred):
-    return 0
+
+def Project(new_schema, parent_schema, parent):
+    # Создается список индексов столбцов в parent, которые соответствуют именам столбцов в new_schema
+    indices = [parent_schema.index(col) for col in new_schema]
+    for row in parent:
+        yield [row[i] for i in indices]
+
 
 def Scan(filename):
     with open(filename, 'r') as f:
@@ -19,14 +19,30 @@ def Scan(filename):
             yield line.strip().split(',')
 
 
-def Project(new_schema, parent_schema, parent):
-    indices = [parent_schema.index(col) for col in new_schema]
-    for row in parent:
-        yield [row[i] for i in indices]
-
 def Print(parent):
     for row in parent:
         print(row)
 
 
-Print(Filter(Eq(1, Value('09:00 AM')), Scan('talks.csv')))
+def Eq(x, y):
+    return lambda row: row[x] == y
+
+
+def Ne(x, y):
+    return lambda row: row[x] != y
+
+
+def Filter(pred, parent):
+    for row in parent:
+        if pred(row):
+            yield row
+
+
+if __name__ == '__main__':
+    # select room, title from talks.csv where time='09:00 AM'
+    filename = 'talks.csv'
+    pred = Eq(1, '09:00 AM')
+    filtered = Filter(pred, filename)
+    result = Project(['room', 'title'], ['time', 'room', 'speaker', 'title'],
+                     Filter(Eq(1, Value('09:00 AM')), Scan('talks.csv')))
+    Print(result)
